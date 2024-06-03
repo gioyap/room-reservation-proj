aa13fefca4671311b91c365c7cd085c69e406f69
MIS-GIO <yapgioedrian@gmail.com>
2024-06-03 11:15:13 +0800

    solve the error during run build

---
+++ app/_document.tsx
@@ -0,0 +1,19 @@
+// app/_document.tsx
+/* eslint-disable @next/next/no-document-import-in-page */
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
