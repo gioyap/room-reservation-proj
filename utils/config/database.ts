8ff510957fa77fbc09e52360c5b760ce9f1e2483
MIS-GIO <yapgioedrian@gmail.com>
2024-06-03 13:26:56 +0800

    fix the database.ts

--- utils/config/database.ts
+++ utils/config/database.ts
@@ -9,22 +9,29 @@ export async function connect() {
 	}
 
 	try {
-		await mongoose.connect(process.env.MONGO_URI!, {});
+		// Check if MONGO_URI environment variable is defined
+		if (!process.env.MONGO_URI) {
+			throw new Error("MONGO_URI environment variable is not defined");
+		}
 
+		// Connect to MongoDB using Mongoose
+		await mongoose.connect(process.env.MONGO_URI, {});
+
+		// Set up event listeners for MongoDB connection
 		mongoose.connection.once("connected", () => {
 			console.log("MongoDB connected");
 			isConnected = true;
 		});
 
 		mongoose.connection.on("error", (err) => {
-			console.log("MongoDB error: " + err);
-			process.exit();
+			console.error("MongoDB error:", err);
+			process.exit(1); // Exit the process with an error code
 		});
 
 		db = mongoose.connection;
 		return db;
 	} catch (error) {
-		console.log(error);
-		process.exit();
+		console.error("Error connecting to MongoDB:", error);
+		process.exit(1); // Exit the process with an error code
 	}
 }
