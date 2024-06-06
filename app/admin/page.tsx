aded9ff1b2a560a1753858bb28408ff8e06b7fc8
MIS-GIO <yapgioedrian@gmail.com>
2024-06-06 16:56:03 +0800

    able to create a better ui for email

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,12 @@
+"use client";
 import Sidebar from "@/components/Sidebar";
 import React from "react";
 
-const AdminDashboard: React.FC = () => {
+const AdminDashboard = () => {
 	return (
 		<div>
 			<Sidebar />
-			<div style={{ textAlign: "center", marginTop: "20px", fontSize: "24px" }}>
-				Still Developing...
-			</div>
+			Still Developing
 		</div>
 	);
 };
