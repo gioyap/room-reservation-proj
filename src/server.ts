789dbc9b0262e52a6d769003b6c3bde0b9b3febc
MIS-GIO <yapgioedrian@gmail.com>
2024-06-13 15:42:57 +0800

    5th try for websocket in admin side

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import http from "http";
 import { Server as SocketIOServer, Socket } from "socket.io";
 import cors from "cors";
-import { connect } from "@/utils/config/database"; // Adjust path as per your project structure
+import { connect } from "@/utils/config/database";
 import Reservation from "@/utils/models/reservation";
 
-// Initialize Express app
 const app = express();
 app.use(
 	cors({
@@ -17,45 +16,43 @@ app.use(
 	})
 );
 
-// Define a route handler for GET /
-app.get("/", (req, res) => {
-	res.send("WebSocket server is running.");
-});
-
-// Create HTTP server
 const server = http.createServer(app);
-
-// Initialize Socket.IO server
 const io = new SocketIOServer(server, {
 	cors: {
 		origin: [
 			"http://localhost:3000",
 			"https://reservation-system-nu.vercel.app",
-			"http://localhost:3000/admin",
-			"https://reservation-system-nu.vercel.app/admin",
 		],
 		methods: ["GET", "POST"],
 	},
 });
 
-// Use the existing MongoDB connection
 connect()
 	.then((db) => {
-		// Socket.IO event listeners
 		io.on("connection", (socket: Socket) => {
 			console.log("New client connected");
 
 			socket.on("reservationUpdated", async (data) => {
 				try {
-					// Update reservation status in MongoDB
 					const updatedReservation = await Reservation.findByIdAndUpdate(
 						data.id,
 						{ status: data.status },
 						{ new: true }
 					);
 
-					// Broadcast updated reservation to all clients
-					io.emit("reservationUpdate", updatedReservation);
+					const pendingReservations = await Reservation.find({
+						status: "Pending",
+					});
+					const acceptedReservations = await Reservation.find({
+						status: "Accepted",
+					});
+					const declinedReservations = await Reservation.find({
+						status: "Declined",
+					});
+
+					io.emit("pendingReservations", pendingReservations);
+					io.emit("acceptedReservations", acceptedReservations);
+					io.emit("declinedReservations", declinedReservations);
 				} catch (error) {
 					console.error("Error updating reservation:", error);
 				}
@@ -68,7 +65,7 @@ connect()
 	})
 	.catch((error) => {
 		console.error("Error connecting to MongoDB:", error);
-		process.exit(1); // Exit the process with an error code
+		process.exit(1);
 	});
 
 export const getSocketIOInstance = () => io;
