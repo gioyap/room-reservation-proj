2bec552cf8c336321bf87a2a3bbace13032cb674
MIS-GIO <yapgioedrian@gmail.com>
2024-06-13 15:53:27 +0800

    6th try websocket in admin side

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,51 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import http from "http";
 import { Server as SocketIOServer, Socket } from "socket.io";
 import cors from "cors";
-import { connect } from "@/utils/config/database";
+import { connect } from "@/utils/config/database"; // Adjust path as per your project structure
 import Reservation from "@/utils/models/reservation";
 
+// Initialize Express app
 const app = express();
 app.use(
 	cors({
 		origin: [
 			"http://localhost:3000",
 			"https://reservation-system-nu.vercel.app",
+			"https://reservation-system-nu.vercel.app/admin/pending",
+			"https://reservation-system-nu.vercel.app/admin/declined",
+			"https://reservation-system-nu.vercel.app/admin/accepted",
 		],
 		methods: ["GET", "POST"],
 	})
 );
 
+// Define a route handler for GET /
+app.get("/", (req, res) => {
+	res.send("WebSocket server is running.");
+});
+
+// Create HTTP server
 const server = http.createServer(app);
+
+// Initialize Socket.IO server
 const io = new SocketIOServer(server, {
 	cors: {
 		origin: [
 			"http://localhost:3000",
 			"https://reservation-system-nu.vercel.app",
+			"https://reservation-system-nu.vercel.app/admin/pending",
+			"https://reservation-system-nu.vercel.app/admin/declined",
+			"https://reservation-system-nu.vercel.app/admin/accepted",
 		],
 		methods: ["GET", "POST"],
 	},
 });
 
+// Use the existing MongoDB connection
 connect()
 	.then((db) => {
+		// Socket.IO event listeners
 		io.on("connection", (socket: Socket) => {
 			console.log("New client connected");
 
@@ -53,6 +70,7 @@ connect()
 					io.emit("pendingReservations", pendingReservations);
 					io.emit("acceptedReservations", acceptedReservations);
 					io.emit("declinedReservations", declinedReservations);
+					io.emit("reservationUpdate", updatedReservation);
 				} catch (error) {
 					console.error("Error updating reservation:", error);
 				}
