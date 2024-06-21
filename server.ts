1a58544ed5fed127d6fe43b8997f44262d3ef156
Gio Yap <gioedrian.yap.l@gmail.com>
2024-06-21 21:48:32 +0800

    fix the gateway error for railway

--- server.ts
+++ server.ts
@@ -8,35 +8,38 @@ import Reservation from "./utils/models/reservation";
 const app = express();
 const httpServer = createServer(app);
 const io = new SocketIOServer(httpServer, {
-	cors: {
-		origin: "https://reservation-system-nu.vercel.app",
-		methods: ["GET", "POST"], // Add methods as needed
-		allowedHeaders: ["Authorization"], // Add allowed headers as needed
-		credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-	},
+    cors: {
+        origin: "https://reservation-system-nu.vercel.app",
+        methods: ["GET", "POST"], // Add methods as needed
+        allowedHeaders: ["Authorization"], // Add allowed headers as needed
+        credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    },
 });
 
 connect();
 
 io.on("connection", (socket: Socket) => {
-	console.log(`Client connected: ${socket.id}`);
+    console.log(`Client connected: ${socket.id}`);
 
-	socket.on("disconnect", () => {
-		console.log(`Client disconnected: ${socket.id}`);
-	});
+    socket.on("disconnect", () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    });
 });
 
 // Watch for changes in Reservation collection
 Reservation.watch().on("change", (change) => {
-	if (
-		change.operationType === "update" &&
-		change.updateDescription.updatedFields.status
-	) {
-		io.emit("newReservationStatus", change.fullDocument);
-	}
+    if (
+        change.operationType === "update" &&
+        change.updateDescription?.updatedFields?.status
+    ) {
+        io.emit("newReservationStatus", change.fullDocument);
+    }
 });
 
-const PORT = process.env.EXPRESS_PORT || 3001;
-httpServer.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}`);
+// Use PORT provided in environment or default to 3000
+const PORT: number = parseInt(process.env.PORT || '3000', 10);
+
+// Listen on `port` and 0.0.0.0
+httpServer.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
