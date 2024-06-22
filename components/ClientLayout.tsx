b3693b865f21c689feb4993048ebc8113bf33a8d
Gio Yap <gioedrian.yap.l@gmail.com>
2024-06-23 00:08:25 +0800

    i remove the font and i install tailwindcss again

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,30 +1,25 @@
 // components/ClientLayout.tsx
-"use client";
-
 import AuthProvider from "../components/Provider";
 import RedirectComponent from "../app/RedirectComponent";
-import { Mulish } from "next/font/google";
 import "../app/globals.css";
 
-const mulish = Mulish({ subsets: ["latin"] });
-
 export default function ClientLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body
-				suppressContentEditableWarning
-				suppressHydrationWarning
-				className={mulish.className}
-			>
-				<AuthProvider>
-					<RedirectComponent />
-					{children}
-				</AuthProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        suppressContentEditableWarning
+        suppressHydrationWarning
+        className="font-sans" // Replace with your desired font class or style
+      >
+        <AuthProvider>
+          <RedirectComponent />
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
 }
