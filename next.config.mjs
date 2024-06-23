8594f86c018401876a8838cb525f71ff499e61c4
Gio Yap <gioedrian.yap.l@gmail.com>
2024-06-23 20:04:58 +0800

    fix the 404 warning

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,16 @@ config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    // Enable source maps only during development
+    if (dev && !isServer) {
+      config.devtool = 'source-map';
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
